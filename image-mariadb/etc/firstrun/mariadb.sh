bdf617868ac2c5ed7b384c6969e8cedefc21f233
juice404error <97803708+juice404error@users.noreply.github.com>
2026-03-18 16:59:54 +0100

    Update mariadb.sh

--- image-mariadb/etc/firstrun/mariadb.sh
+++ image-mariadb/etc/firstrun/mariadb.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
-# Verzió lekérése a Dockerfile-ból (vagy környezeti változóból)
-GUAC_VER=${GUAC_VER:-"1.5.5"}
+GUAC_VER="1.0.0"
 
 MYSQL_CONFIG=/etc/my.cnf.d/mariadb-server.cnf
 MYSQL_SCHEMA=/opt/guacamole/mysql/schema
 MYSQL_DATABASE=/config/databases
 
-# Könyvtárak előkészítése
-mkdir -p /var/run/mysqld /var/log/mysql /config/databases
-chown -R abc:abc /var/log/mysql /var/run/mysqld /config/databases
-chmod -R 777 /var/run/mysqld
+sed -i '/\[mysqld\]/a user= '"$PUID"'' "$MYSQL_CONFIG"
+mkdir -p /var/run/mysqld /var/log/mysql
+chown -R abc:abc var/log/mysql /var/lib/mysql /var/run/mysqld
+chmod -R 777 /var/log/mysql /var/lib/mysql /var/run/mysqld
 
 start_mysql() {
-  echo "Starting MariaDB..."
-  /usr/bin/mysqld_safe --datadir="$MYSQL_DATABASE" > /dev/null 2>&1 &
+  echo "Starting MariaDB."
+  /usr/bin/mysqld_safe > /dev/null 2>&1 &
   RET=1
   while [[ RET -ne 0 ]]; do
       mysql -uroot -e "status" > /dev/null 2>&1
@@ -24,41 +23,82 @@ start_mysql() {
 }
 
 stop_mysqld() {
-  echo "Stopping MariaDB..."
+  echo "Stopping MariaDB."
   mysqladmin -u root shutdown
   sleep 3
 }
 
-# Adatbázis inicializálása, ha még nem létezik
-if [ ! -d "$MYSQL_DATABASE"/guacamole ]; then
+upgrade_database() {
+  local UPG_VER="$1"
+  start_mysql
+  echo "Upgrading database pre-$UPG_VER."
+  echo "$GUAC_VER" > "$MYSQL_DATABASE"/guacamole/version
+  mysql -uroot guacamole < ${MYSQL_SCHEMA}/upgrade/upgrade-pre-${UPG_VER}.sql
+  stop_mysqld
+  echo "Upgrade complete."
+}
+
+# If databases do not exist, create them
+if [ -f "$MYSQL_DATABASE"/guacamole/guacamole_user.ibd ]; then
+  echo "Database exists."
+  if [ -f "$MYSQL_DATABASE"/guacamole/version ]; then
+    OLD_GUAC_VER=$(cat $MYSQL_DATABASE/guacamole/version)
+    IFS="."
+    read -ra OLD_SPLIT <<< "$OLD_GUAC_VER"
+    read -ra NEW_SPLIT <<< "$GUAC_VER"
+    IFS=" "
+    if (( NEW_SPLIT[2] > OLD_SPLIT[2] )) || (( NEW_SPLIT[1] > OLD_SPLIT[1] )) || (( NEW_SPLIT[0] > OLD_SPLIT[0] )); then
+      echo "Database being upgraded."
+      case $OLD_GUAC_VER in
+      "0.9.13")
+        upgrade_database "0.9.14"
+        upgrade_database "1.0.0"
+        ;;
+      "0.9.14")
+        upgrade_database "1.0.0"
+        ;;
+      esac
+    elif (( OLD_SPLIT[2] > NEW_SPLIT[2] )) || (( OLD_SPLIT[1] > NEW_SPLIT[1] )) || (( OLD_SPLIT[0] > NEW_SPLIT[0] )); then
+      echo "Database newer revision, no change needed."
+      # revert 1.1.0 back to 1.0.0 to match database version rather than actual version since there have been no changes.
+      echo "$GUAC_VER" > "$MYSQL_DATABASE"/guacamole/version
+    else
+      echo "Database upgrade not needed."
+    fi
+  else
+    echo "Database being upgraded."
+    upgrade_database "0.9.13"
+    upgrade_database "0.9.14"
+    upgrade_database "1.0.0"
+  fi
+else
   if [ -f /config/guacamole/guacamole.properties ]; then
-    echo "Initializing Guacamole database in $MYSQL_DATABASE"
-    /usr/bin/mysql_install_db --user=abc --datadir="$MYSQL_DATABASE"
-    
+    echo "Initializing Guacamole database."
+    /usr/bin/mysql_install_db --datadir="$MYSQL_DATABASE"
+    echo "Database installation complete."
     start_mysql
-    
-    echo "Creating database and user..."
-    mysql -uroot -e "CREATE DATABASE IF NOT EXISTS guacamole"
-    
-    # Jelszó kinyerése a properties fájlból
-    PW=$(grep "mysql-password:" /config/guacamole/guacamole.properties | awk '{print $2}')
-    
-    mysql -uroot -e "CREATE USER IF NOT EXISTS 'guacamole'@'localhost' IDENTIFIED BY '$PW'"
-    mysql -uroot -e "GRANT ALL PRIVILEGES ON guacamole.* TO 'guacamole'@'localhost'"
+    echo "Creating Guacamole database."
+    mysql -uroot -e "CREATE DATABASE guacamole"
+    echo "Creating Guacamole database user."
+    PW=$(cat /config/guacamole/guacamole.properties | grep -m 1 "mysql-password:\s" | sed 's/mysql-password:\s//')
+    mysql -uroot -e "CREATE USER 'guacamole'@'localhost' IDENTIFIED BY '$PW'"
+    echo "Database created. Granting access to 'guacamole' user for localhost."
+    mysql -uroot -e "GRANT SELECT,INSERT,UPDATE,DELETE ON guacamole.* TO 'guacamole'@'localhost'"
     mysql -uroot -e "FLUSH PRIVILEGES"
-    
-    echo "Loading schema..."
+    echo "Creating Guacamole database schema and default admin user."
     mysql -uroot guacamole < ${MYSQL_SCHEMA}/001-create-schema.sql
     mysql -uroot guacamole < ${MYSQL_SCHEMA}/002-create-admin-user.sql
-    
     echo "$GUAC_VER" > "$MYSQL_DATABASE"/guacamole/version
     stop_mysqld
+    echo "Setting database file permissions"
+    chown -R abc:abc /config/databases
+    chmod -R 755 /config/databases
+    echo "Removing mysql-server logrotate directive"
+    rm /etc/logrotate.d/mysql-server
+    sleep 3
     echo "Initialization complete."
   else
-    echo "Error: guacamole.properties not found! Skipping DB init."
+    echo "Error! Unable to create database. guacamole.properties file does not exist."
+    echo "If you see this error message please contact support in the unRAID forums: https://forums.unraid.net/topic/54855-support-jasonbean-apache-guacamole/"
   fi
 fi
-
-# Indítás supervisord-on keresztül (előtérben tartva)
-echo "Starting MariaDB in foreground..."
-exec /usr/bin/mysqld_safe --datadir="$MYSQL_DATABASE"
