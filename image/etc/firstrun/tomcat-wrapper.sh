20d74ebbd77586bf01f0fd8a39bf492c55130f37
juice404error <97803708+juice404error@users.noreply.github.com>
2026-03-20 08:39:11 +0100

    Update tomcat-wrapper.sh

--- image/etc/firstrun/tomcat-wrapper.sh
+++ image/etc/firstrun/tomcat-wrapper.sh
@@ -1,24 +1,11 @@
 #!/bin/bash
-# Source: http://serverfault.com/questions/425132/controlling-tomcat-with-supervisor
-function shutdown()
-{
-    date
-    echo "Shutting down Tomcat"
-    unset CATALINA_PID # Necessary in some cases
-    $CATALINA_HOME/bin/catalina.sh stop
-}
+echo "Starting Tomcat..."
 
-date
-echo "Starting Tomcat"
-export JAVA_HOME="/usr/lib/jvm/default-jvm"
-export CATALINA_HOME="/opt/tomcat"
-export CATALINA_BASE="/var/lib/tomcat"
-export CATALINA_PID="/var/run/tomcat/tomcat.pid"
+export CATALINA_OPTS="-Xms512M -Xmx1024M -server -XX:+UseParallelGC"
 
-. $CATALINA_HOME/bin/catalina.sh start
+# Log könyvtár biztosítása
+mkdir -p /config/log/tomcat
+chown -R abc:abc /config/log/tomcat
 
-# Allow any signal which would kill a process to stop Tomcat
-trap shutdown HUP INT QUIT ABRT KILL ALRM TERM TSTP
-
-echo "Waiting for `cat $CATALINA_PID`"
-wait `cat $CATALINA_PID`
+# Tomcat indítása (run parancs az előtérben tartja)
+exec ${CATALINA_HOME}/bin/catalina.sh run
