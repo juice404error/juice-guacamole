4c715c3e4b27ca82b341462ceeb8814fdd094595
juice404error <97803708+juice404error@users.noreply.github.com>
2026-03-21 17:13:12 +0100

    Update firstrun.sh

--- image/etc/firstrun/firstrun.sh
+++ image/etc/firstrun/firstrun.sh
@@ -1,30 +1,29 @@
 #!/bin/bash
 echo "--- Initializing Guacamole Environment ---"
 
+# 1. User beállítása - Kényszerítjük a 1000-es ID-t, ha nem jönne környezeti változó
+PUID=${PUID:-1000}
+PGID=${PGID:-100}
+
+groupmod -o -g "$PGID" abc
+usermod -o -u "$PUID" abc
+
+echo "----------------------"
+echo "User UID: $(id -u abc)"
+echo "User GID: $(id -g abc)"
+echo "----------------------"
+
 # Sablonok másolása
 if [ ! -f "/config/guacamole/guacamole.properties" ]; then
     echo "Creating properties from template..."
+    mkdir -p /config/guacamole
     cp /etc/firstrun/templates/* /config/guacamole/
 fi
 
-# Logback.xml kinyerése - PONTOSÍTVA
+# Logback.xml kinyerése
 if [ ! -f "/config/guacamole/logback.xml" ]; then
     echo "Extracting logback.xml..."
-    # A ROOT.war-ból (ami a guacamole.war) kinyerjük a logback.xml-t
     unzip -o -j /opt/guacamole/guacamole.war "WEB-INF/classes/logback.xml" -d "/config/guacamole/" > /dev/null 2>&1
-    
-    # Ha még így sem találja, létrehozunk egy alapértelmezettet, hogy ne legyen hiba
-    if [ ! -f "/config/guacamole/logback.xml" ]; then
-        echo "Warning: Could not extract logback.xml, creating default."
-        cat <<EOF > /config/guacamole/logback.xml
-<configuration>
-    <appender name="STDOUT" class="ch.qos.logback.core.ConsoleAppender">
-        <encoder><pattern>%d{HH:mm:ss.SSS} [%thread] %-5level %logger{36} - %msg%n</pattern></encoder>
-    </appender>
-    <root level="info"><appender-ref ref="STDOUT" /></root>
-</configuration>
-EOF
-    fi
 fi
 
 # MySQL Sémák szinkronizálása
@@ -36,5 +35,6 @@ if [ "$OPT_MYSQL" = "Y" ]; then
     cp /opt/guacamole/mysql/*.jar /config/guacamole/extensions/
 fi
 
-chown -R abc:users /config/guacamole /config/mysql-schema
+# CSAK a specifikus mappákat írjuk át, nem a teljes /config-ot!
+chown -R abc:users /config/guacamole /config/mysql-schema /opt/tomcat /var/run/tomcat
 echo "--- Initialization Finished ---"
