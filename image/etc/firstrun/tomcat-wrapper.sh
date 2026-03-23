d32cd29df56c598b65b5b212b296a7604169ec92
juice404error <97803708+juice404error@users.noreply.github.com>
2026-03-23 10:13:53 +0100

    Update tomcat-wrapper.sh

--- image/etc/firstrun/tomcat-wrapper.sh
+++ image/etc/firstrun/tomcat-wrapper.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
-echo "Starting Tomcat..."
+function shutdown()
+{
+    date
+    echo "Shutting down Tomcat"
+    unset CATALINA_PID # Necessary in some cases
+    $CATALINA_HOME/bin/catalina.sh stop
+}
 
+date
+echo "Starting Tomcat..."
+export JAVA_HOME="/usr/lib/jvm/default-jvm"
+export CATALINA_HOME="/opt/tomcat"
+export CATALINA_BASE="/var/lib/tomcat"
+export CATALINA_PID="/var/run/tomcat/tomcat.pid"
 export CATALINA_OPTS="-Xms512M -Xmx1024M -server -XX:+UseParallelGC"
 
 # Log könyvtár biztosítása
@@ -9,3 +21,8 @@ chown -R abc:abc /config/log/tomcat
 
 # Tomcat indítása (run parancs az előtérben tartja)
 exec ${CATALINA_HOME}/bin/catalina.sh run
+
+trap shutdown HUP INT QUIT ABRT KILL ALRM TERM TSTP
+
+echo "Waiting for `cat $CATALINA_PID`"
+wait `cat $CATALINA_PID`
