93eaf85aa772c0c7d93e9794c2560241cab99658
juice404error <97803708+juice404error@users.noreply.github.com>
2026-03-25 10:33:59 +0100

    Update firstrun.sh

--- image/etc/firstrun/firstrun.sh
+++ image/etc/firstrun/firstrun.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 echo "--- Initializing Guacamole Environment ---"
 
-# Könyvtárak kényszerített létrehozása
 mkdir -p /config/guacamole/extensions /config/guacamole/lib /config/log/tomcat /config/mysql-schema
 
-# Sablonok másolása
 if [ ! -f "/config/guacamole/guacamole.properties" ]; then
     echo "Creating properties from template..."
     cp /etc/firstrun/templates/* /config/guacamole/
 fi
 
-# Logback.xml kinyerése
 if [ ! -f "/config/guacamole/logback.xml" ]; then
     echo "Extracting logback.xml..."
     unzip -o -j /opt/guacamole/guacamole.war "WEB-INF/classes/logback.xml" -d "/config/guacamole/" > /dev/null 2>&1
-    
     if [ ! -f "/config/guacamole/logback.xml" ]; then
         echo "Creating default logback.xml..."
         cat <<EOF > /config/guacamole/logback.xml
@@ -28,22 +24,19 @@ EOF
     fi
 fi
 
-# Log szint beállítása
 sed -i 's/ level="[^"]*"/ level="'$LOGBACK_LEVEL'"/' /config/guacamole/logback.xml
 
-# MySQL/MariaDB szinkronizálás
 if [ "$OPT_MYSQL" = "Y" ]; then
-    echo "Syncing MySQL extensions, JDBC driver and setting permissions..."
+    echo "Syncing MySQL extensions and JDBC driver..."
+    # A sémákat mindig felülírjuk az aktuálisra a konténerből
     cp -R /opt/guacamole/mysql/schema/* /config/mysql-schema/
+    # Az extension-öket és a libeket is szinkronizáljuk
     cp /opt/guacamole/mysql/*.jar /config/guacamole/extensions/
-    # Driver másolása a lib mappába
     cp /opt/guacamole/mysql/lib/*.jar /config/guacamole/lib/ 2>/dev/null
     
-    # Jogosultságok
     chmod +x /config/guacamole/extensions/*.jar
     chmod +x /config/guacamole/lib/*.jar 2>/dev/null
 fi
 
-# Jogosultságok véglegesítése
 chown -R abc:abc /config/guacamole /config/mysql-schema /config/log/tomcat
 echo "--- Initialization Finished ---"
